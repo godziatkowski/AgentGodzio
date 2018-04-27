9e4060c4cafa7e7f254dcb7dfd17a2f49c7d31c5
godzio <godziatkowski.jan@gmail.com>
2018-04-27 23:03:34 +0200

    Init backend project

---
+++ AgentGodzioServer/src/main/java/pl/godziatkowski/AgentGodzioServer/AgentGodzioServerApplication.java
@@ -0,0 +1,12 @@
+package pl.godziatkowski.AgentGodzioServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentGodzioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentGodzioServerApplication.class, args);
+	}
+}
