6329675d4569396fa443805da0102d79faf8520c
godzio <godziatkowski.jan@gmail.com>
2018-04-29 00:01:13 +0200

    Item domain update

--- AgentGodzioServer/src/main/java/pl/godziatkowski/AgentGodzioServer/domain/item/Item.java
+++ AgentGodzioServer/src/main/java/pl/godziatkowski/AgentGodzioServer/domain/item/Item.java
@@ -1,8 +1,25 @@
 package pl.godziatkowski.AgentGodzioServer.domain.item;
 
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@Data
+@Entity
 class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
     private String category;
+
+    ItemDto toDto(){
+        return new ItemDto(id, name, description, category);
+    }
 }
--- AgentGodzioServer/src/main/java/pl/godziatkowski/AgentGodzioServer/domain/item/ItemDto.java
+++ AgentGodzioServer/src/main/java/pl/godziatkowski/AgentGodzioServer/domain/item/ItemDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ItemDto {
-    private final Long id;
+    private final long id;
     private final String name;
     private final String description;
     private final String category;
